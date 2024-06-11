8129b33aaaf096fb0236846b697c58d9460f3b2d
Tajariel <62051015+Tajariel@users.noreply.github.com>
2024-06-11 12:17:48 +0200

    Fixed boat buoyancy

--- Assets/Script/BoatBuoyancy.cs
+++ Assets/Script/BoatBuoyancy.cs
@@ -13,7 +13,7 @@ public class BoatBuoyancy : MonoBehaviour
     private void FixedUpdate(){
         rigidbody.AddForceAtPosition(Physics.gravity/floaterCount, transform.position,ForceMode.Acceleration);
         float waveHeight = WaveManager.instance.GetWaveHeight(transform.position.x);
-        if(transform.position.y <0f){
+        if(transform.position.y <waveHeight){
             float displacementMultiplier = Mathf.Clamp01((waveHeight-transform.position.y)/depthBeforeSubmerged) * displacementAmount;
             rigidbody.AddForceAtPosition(new Vector3(0f, Mathf.Abs(Physics.gravity.y)*displacementMultiplier,0f),transform.position, ForceMode.Acceleration);
             rigidbody.AddForce(displacementMultiplier*-rigidbody.velocity*waterDrag*Time.fixedDeltaTime,ForceMode.VelocityChange);
