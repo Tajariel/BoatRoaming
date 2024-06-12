17cc2eed487f81463e9a393c1359d4ef3ae190d3
Tajariel <62051015+Tajariel@users.noreply.github.com>
2024-06-12 11:14:06 +0200

    Fixed Input and Controller

--- Assets/Script/BoatController.cs
+++ Assets/Script/BoatController.cs
@@ -7,7 +7,6 @@ public class BoatController : MonoBehaviour
     public Rigidbody rb;
     public float maxSpeed = 10;
     private float currentSpeed = 0;
-    public float accel = 10;
     public float rotationSpeed = 0.5f;
     
 
@@ -20,11 +19,11 @@ public class BoatController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButton("Jump")){
-            currentSpeed = (currentSpeed+accel)*Time.deltaTime;
-            rb.AddForce(maxSpeed*transform.right, ForceMode.Force);            
+        if(Input.GetAxis("Fire1")!=0){
+            float accel = Input.GetAxis("Fire1");
+            rb.AddForce(maxSpeed*transform.right*accel, ForceMode.Force);            
         }
-        if(Input.GetButton("Horizontal")){
+        if(Input.GetAxis("Horizontal")!= 0){
             float turn = Input.GetAxis("Horizontal");
             rb.AddTorque(transform.up*rotationSpeed*turn);
         }
